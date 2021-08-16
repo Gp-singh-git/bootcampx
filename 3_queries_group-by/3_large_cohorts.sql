942964b5b884ece305f74eea974ee79d1eb5cd53
gurpreet singh <gurpreet212004@gmail.com>
2021-08-16 20:04:14 +0000

    Queries Group by

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name, count(students.id) AS student_count
+FROM cohorts
+JOIN students
+ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.id) >= 18
+ORDER BY student_count;
